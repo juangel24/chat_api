7ac697286c3bd12964b723d64bf51fbe924a48d8
iovassoto@gmail.com <iovassoto@gmail.com>
2020-06-23 20:25:59 -0500

    socket and chatcontroller

---
+++ app/Controllers/Ws/ChatController.js
@@ -0,0 +1,13 @@
+'use strict'
+
+class ChatController {
+  constructor ({ socket, request }) {
+    this.socket = socket
+    this.request = request
+  }
+  onMessage(data) {
+    console.log(this.socket.id);
+  }
+}
+
+module.exports = ChatController
--- server.js
+++ server.js
@@ -17,9 +17,12 @@
 |     Make sure to pass a relative path from the project root.
 */
 
-const { Ignitor } = require('@adonisjs/ignitor')
+const {
+  Ignitor
+} = require('@adonisjs/ignitor')
 
 new Ignitor(require('@adonisjs/fold'))
   .appRoot(__dirname)
   .fireHttpServer()
+  .wsServer()
   .catch(console.error)

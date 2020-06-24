24dd1458c8989300066442bd0444fbe28ae62748
Juan Angel <juangel2406@gmail.com>
2020-06-24 00:44:33 -0500

    Message migration

---
+++ database/migrations/1592974030416_messages_schema.js
@@ -0,0 +1,21 @@
+'use strict'
+
+/** @type {import('@adonisjs/lucid/src/Schema')} */
+const Schema = use('Schema')
+
+class MessagesSchema extends Schema {
+  up () {
+    this.create('messages', (table) => {
+      table.increments()
+      table.string('text').notNullable()
+      table.integer('sender_id').notNullable().references('id').inTable('users')
+      table.timestamps()
+    })
+  }
+
+  down () {
+    this.drop('messages')
+  }
+}
+
+module.exports = MessagesSchema

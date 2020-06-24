8e7745efc6d3cb61ee657e83c30a423cf1d2e541
Juan Angel <juangel2406@gmail.com>
2020-06-24 02:33:26 -0500

    message saving

--- app/Controllers/Ws/ChatController.js
+++ app/Controllers/Ws/ChatController.js
@@ -1,4 +1,5 @@
 'use strict'
+const Message = use('App/Models/Message')
 const users_array = []
 
 class ChatController {
@@ -8,12 +9,18 @@ class ChatController {
   }
 
   onMessage(data) {
-    this.socket.broadcastToAll('data', data)
+    const msg = new Message()
+
+    msg.text = data.text
+    msg.sender_id = data.sender_id
+    msg.save()
+
+    this.socket.broadcastToAll('data', Message.pickInverse(3))
   }
 
   onNewUser(user){
     console.log(user);
-    
+
     if (user.length !== null || user.length !== '' || user.length !== 0) {
       users_array.push(user)
       this.socket.broadcast('newUser', users_array)

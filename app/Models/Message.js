a913f1aa617f7ad58a3a8cd1aabf00dab368ac81
Juan Angel <juangel2406@gmail.com>
2020-06-24 02:33:04 -0500

    Message Model

---
+++ app/Models/Message.js
@@ -0,0 +1,21 @@
+'use strict'
+
+/** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
+const Model = use('Model')
+const Encryption = use('Encryption')
+
+class Message extends Model {
+  user () {
+    return this.belongsTo('App/Models/User')
+  }
+
+  static boot () {
+    super.boot()
+
+    this.addHook('beforeSave', async (messageInstance) => {
+      messageInstance.text = await Encryption.encrypt(messageInstance)
+    })
+  }
+}
+
+module.exports = Message
